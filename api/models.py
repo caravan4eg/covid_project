3574797619e27ed0efb84058a0569f7a228a5cf6
caravan4eg <alex.apanasik@gmail.com>
2020-04-18 21:51:31 +0300

    setup Fact model and admin panel

--- api/models.py
+++ api/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Facts(models.Model):
+class Fact(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    disease_name = models.CharField(max_length=250)
     city = models.CharField(max_length=250)
     rayon = models.CharField(max_length=250)
     oblast = models.CharField(max_length=250)
@@ -11,6 +14,9 @@ class Facts(models.Model):
     tests_made = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.created_at} {self.disease_name}'
 
     # sick_day_changes = models.IntegerField()  # изменение числа заболевших за сутки
     # recovered_day_changes = models.IntegerField()  # выздоровело
--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'api',
+    # 3rd-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# new
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        ]
+}
