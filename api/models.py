904b7365a742c3a003d4e5be0c19d01f0ae93d24
caravan4eg <alex.apanasik@gmail.com>
2020-04-19 00:16:14 +0300

    models for posts and projects added

--- api/models.py
+++ api/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 
 class Fact(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    where_is_it = models.ForeignKey('Location', on_delete=models.CASCADE)
-    disease_name = models.CharField(max_length=250, verbose_name = 'Название болезни')
-    sick = models.IntegerField(verbose_name = 'Количество заболевших')  # заболело
-    dead = models.IntegerField(verbose_name = 'Количество умерших')  # умерло
-    recovered = models.IntegerField(verbose_name = 'Количество выздоровевших')  # выздоровело
-    tests_made = models.IntegerField(verbose_name = 'Сделано тестов')
+    where_is_it = models.ForeignKey('Location',
+                                    on_delete=models.CASCADE)
+    disease_name = models.CharField(
+        max_length=250,
+        verbose_name='Название болезни')
+    sick = models.IntegerField(verbose_name='Количество заболевших')
+    dead = models.IntegerField(verbose_name='Количество умерших')
+    recovered = models.IntegerField(verbose_name='Количество выздоровевших')
+    tests_made = models.IntegerField(verbose_name='Сделано тестов')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -22,13 +25,44 @@ class Fact(models.Model):
 
 
 class Location(models.Model):
-    place = models.CharField(max_length=250, verbose_name = 'Населенный пункт')
-    rayon = models.CharField(max_length=250, verbose_name = 'Район')
-    oblast = models.CharField(max_length=250, verbose_name = 'Область')
+    place = models.CharField(max_length=250,
+                             verbose_name='Населенный пункт')
+    rayon = models.CharField(max_length=250,
+                             verbose_name='Район')
+    oblast = models.CharField(max_length=250,
+                              verbose_name='Область')
 
     def __str__(self):
-        return f'{self.place} / {self.rayon} / {self.oblast}'
+        return f'{self.place}'
 
     class Meta:
         verbose_name = 'Населенный пункт'
         verbose_name_plural = 'Населенные пункты'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    src_logo = models.ImageField(upload_to='logos/%Y/%m/%d/')
+    src_name = models.CharField(max_length=250)
+    src_url = models.URLField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to='logos/%Y/%m/%d/')
+    title = models.CharField(max_length=250)
+    url = models.URLField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
